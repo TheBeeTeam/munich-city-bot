78e15a9deb4f97193d8f4cf026c2c16252cce2a4
Adrian Hernandez-Mendez <adrian.hernandez@tum.de>
2016-11-12 00:32:40 +0100

    Adding the message function

--- src/routes/botRouter.js
+++ src/routes/botRouter.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const express   = require('express');
+const http      = require('http');
+
 
 var router = express.Router();
 
@@ -15,10 +17,38 @@ router.get('/', (req, res) => {
 
 router.post('/', (req, res) => {
 
-    return res.json({method: 'POST', description: 'Chat Bot Integration'});
+    return sendMessage('test message').then((data) => {
+        return res.json(data);
+    }).catch((err) => {
+        console.log(err);
+    });
 
 });
 
+
+
+
+
+
+function sendMessage(msg) {
+    return new Promise(function(resolve, reject) {
+
+        let options = {
+            host: 'api.telegram.org',
+            port: 80,
+            path: '/bot298030970:AAHzKDBQ2wo0rnA6xwuRuxnCK0Bk8uKMO1w/sendMessage?chat_id=-178955930&text=' + msg
+        };
+
+        http.get(options, (resp) => {
+            resp
+                .on('data',(data) => {
+                        return resolve(data);
+                 })
+                .on("error", (error) => reject ({message: "Got error: " + error.message}));
+        });
+
+    })
+}
 
 
 module.exports = router;
