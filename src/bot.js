118d465e43b283c202cc76a375f68d100ae737a9
Daniel Braun <daniel.braun@tum.de>
2016-11-12 13:43:35 +0100

    luis query test

--- src/bot.js
+++ src/bot.js
@@ -16,8 +16,8 @@ bot.on('message', (message) => {
     let text = message.text;
     let user = message.from.username || message.from.first_name;
 	
-	let luisResp = luis.analyseMessage(text);
-	bot.sendMessage(chatId,luisResp);
+	let luisResp = luis.analyseMessage(text).then(resp => {bot.sendMessage(chatId,"SEND")});
+	
 	
     let msg = `${user} send the message: ${text}` ;
 	
