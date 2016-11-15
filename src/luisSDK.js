df26e22444b8150b7188d773ea8253e7bc75bc32
Daniel Braun <daniel.braun@tum.de>
2016-11-15 17:10:22 +0100

    fixed minutes calculation

--- src/luisSDK.js
+++ src/luisSDK.js
@@ -28,8 +28,7 @@ module.exports = class LuisSDK extends EventEmitter {
   		s = (s - ms) / 1000;
   		var secs = s % 60;
   		s = (s - secs) / 60;
-  		var mins = s / 60;
-  		return mins;
+  		return s;
 	}
 
     constructor(token, key) {
